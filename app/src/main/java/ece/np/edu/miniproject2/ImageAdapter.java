f9a388175f2296be1bb598b8b514b46cb1abcb77
HalozyR <iikodachi6@gmail.com>
2021-01-23 12:23:35 +0800

    Added ImageAdapter class, Added ForgotPasswordActivity & layout, Updated UploadImage class, Updated ImageActivity

---
+++ app/src/main/java/ece/np/edu/miniproject2/ImageAdapter.java
@@ -0,0 +1,122 @@
+package ece.np.edu.miniproject2;
+
+import android.content.Context;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import ece.np.edu.miniproject2.databinding.ImageItemBinding;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
+
+    ImageItemBinding binding;
+
+    private final static String TAG = "ImageAdapter";
+    private Context context;
+    private List<UploadImage> uploads;
+    private onItemClickListener mListener;
+
+    public ImageAdapter(Context context, List<UploadImage> uploads) {
+        this.context = context;
+        this.uploads = uploads;
+    }
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.image_item, parent, false);
+        return new ImageViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+        UploadImage uploadCurrent = uploads.get(position);
+        holder.tvName.setText(uploadCurrent.getFileName());
+        Picasso.get()
+                .load(uploadCurrent.getImageUrl())
+                .placeholder(R.mipmap.ic_launcher)
+                .fit()
+                .centerCrop()
+                .into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return uploads.size();
+    }
+
+    public class ImageViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener,
+            View.OnCreateContextMenuListener, MenuItem.OnMenuItemClickListener{
+
+        public TextView tvName;
+        public ImageView imageView;
+        public ImageViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvName = itemView.findViewById(R.id.tvImageUploadName);
+            imageView = itemView.findViewById(R.id.ivImageUpload);
+
+            itemView.setOnClickListener(this);
+            itemView.setOnCreateContextMenuListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (mListener != null){
+                int position = getAdapterPosition();
+                if(position != RecyclerView.NO_POSITION){
+                    mListener.onSelectClick(position);
+                }
+            }
+        }
+
+        @Override
+        public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+            menu.setHeaderTitle("Select Action");
+            MenuItem selectItem = menu.add(Menu.NONE, 1, 1, "Select Image");
+            MenuItem deleteItem = menu.add(Menu.NONE, 2, 2, "Delete");
+
+            selectItem.setOnMenuItemClickListener(this);
+            deleteItem.setOnMenuItemClickListener(this);
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            if (mListener != null){
+                int position = getAdapterPosition();
+                if(position != RecyclerView.NO_POSITION){
+                    switch (item.getItemId()){
+                        case 1:
+                            mListener.onSelectClick(position);
+                            return true;
+                        case 2:
+                            mListener.onDeleteClick(position);
+                            return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+    public interface onItemClickListener{
+        void onSelectClick(int position);
+        void onDeleteClick(int position);
+    }
+
+    public void setOnItemClickListener(onItemClickListener listener){
+        mListener = listener;
+    }
+}
